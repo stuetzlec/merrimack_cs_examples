6607fb57201424ac02e456f258c206a55d552a45
Christopher Stuetzle <stuetzlec@merrimack.edu>
2024-01-29 13:29:52 -0500

    Added correct file structure to example.

---
+++ CSC2620_OOD/Code Examples/Unit2_DebuggingExample/src/csc2620_debugging/CSC2620_DebuggingExample.java
@@ -0,0 +1,18 @@
+package csc2620_debugging;
+
+/**
+ *
+ * @author stuetzlec
+ */
+public class CSC2620_DebuggingExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TestClass newone = new TestClass(10, "hello");
+
+        System.out.println(newone.tester);
+    }
+    
+}
