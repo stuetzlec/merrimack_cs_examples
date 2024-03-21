7a685cf471c0b1e1696dc0f73d3bac2aafa9823d
Christopher Stuetzle <stuetzlec@merrimack.edu>
2024-03-21 09:34:39 -0400

    Adding yet another MVC Example

---
+++ CSC2620_OOD/Code Examples/Unit6_MVCExample2/StudentView.java
@@ -0,0 +1,7 @@
+class StudentView {
+	public void printStudentDetails(String studentName, String studentRollNo) {
+		System.out.println("Student:");
+		System.out.println("Name: " + studentName);
+		System.out.println("Roll No: " + studentRollNo);
+	}
+}
